a327ef2b6e4970ff717c2ce11d8ce8209a27f271
Pierre Tessier <pierre@pierretessier.com>
2024-02-16 00:14:16 -0500

    add otlp common functions

--- route/otlp_trace.go
+++ route/otlp_trace.go
@@ -7,8 +7,6 @@ import (
 	"net/http"
 
 	huskyotlp "github.com/honeycombio/husky/otlp"
-	"github.com/honeycombio/refinery/types"
-
 	collectortrace "go.opentelemetry.io/proto/otlp/collector/trace/v1"
 )
 
@@ -36,7 +34,7 @@ func (r *Router) postOTLPTrace(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if err := processTraceRequest(req.Context(), r, result.Batches, ri.ApiKey); err != nil {
+	if err := processOtlpRequest(req.Context(), r, result.Batches, ri.ApiKey); err != nil {
 		r.handlerReturnWithError(w, ErrUpstreamFailed, err)
 	}
 }
@@ -62,49 +60,9 @@ func (t *TraceServer) Export(ctx context.Context, req *collectortrace.ExportTrac
 		return nil, huskyotlp.AsGRPCError(err)
 	}
 
-	if err := processTraceRequest(ctx, t.router, result.Batches, ri.ApiKey); err != nil {
+	if err := processOtlpRequest(ctx, t.router, result.Batches, ri.ApiKey); err != nil {
 		return nil, huskyotlp.AsGRPCError(err)
 	}
 
 	return &collectortrace.ExportTraceServiceResponse{}, nil
-}
-
-func processTraceRequest(
-	ctx context.Context,
-	router *Router,
-	batches []huskyotlp.Batch,
-	apiKey string) error {
-
-	var requestID types.RequestIDContextKey
-	apiHost, err := router.Config.GetHoneycombAPI()
-	if err != nil {
-		router.Logger.Error().Logf("Unable to retrieve APIHost from config while processing OTLP batch")
-		return err
-	}
-
-	// get environment name - will be empty for legacy keys
-	environment, err := router.getEnvironmentName(apiKey)
-	if err != nil {
-		return nil
-	}
-
-	for _, batch := range batches {
-		for _, ev := range batch.Events {
-			event := &types.Event{
-				Context:     ctx,
-				APIHost:     apiHost,
-				APIKey:      apiKey,
-				Dataset:     batch.Dataset,
-				Environment: environment,
-				SampleRate:  uint(ev.SampleRate),
-				Timestamp:   ev.Timestamp,
-				Data:        ev.Attributes,
-			}
-			if err = router.processEvent(event, requestID); err != nil {
-				router.Logger.Error().Logf("Error processing event: " + err.Error())
-			}
-		}
-	}
-
-	return nil
 }
